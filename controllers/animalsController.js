e9fc6e1c17756e229467255a77d58ca801966984
Subaru Goto <subaru.dayo@gmail.com>
2024-01-26 15:06:15 +0100

    fix error handling

--- controllers/animalsController.js
+++ controllers/animalsController.js
@@ -2,27 +2,19 @@ import { typePage } from "../components/typePage.js";
 import { petPage } from "../components/petPage.js";
 
 export const handleAnimalRoutes = (req, res) => {
-  if(req.params) {
-    try{
+  try{
       const { pet_type } = req.params;
       res.send(typePage(pet_type));
     } catch(error) {
       res.status(404).send("404 No pet found");
     }
-  } else {
-    res.status(404).send("404 No pet found");
-  };
 };
 
 export const handlePetRoutes = (req, res) => {
-  if(req.params?.pet_id) {
-    try{
-      const { pet_type, pet_id } = req.params;
-      res.send(petPage(pet_type, pet_id));
-    } catch(error) {
-      res.status(404).send("404 No pet found");
-    }
-  } else {
+  try{
+    const { pet_type, pet_id } = req.params;
+    res.send(petPage(pet_type, pet_id));
+  } catch(error) {
     res.status(404).send("404 No pet found");
-  };
-}
+  }
+} 
